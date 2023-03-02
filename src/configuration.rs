570d8e5ed912ac81ae12d0d457d25c28588d9c98
eulerdisk <eulerdisk@gmail.com>
2023-03-02 15:30:13 +0100

    First commit.

---
+++ src/configuration.rs
@@ -0,0 +1,48 @@
+use secrecy::{ExposeSecret, Secret};
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: secrecy::Secret<String>,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> Secret<String> {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port,
+            self.database_name
+        )
+        .into()
+    }
+
+    pub fn connection_string_without_db(&self) -> Secret<String> {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port
+        )
+        .into()
+    }
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let mut settings = config::Config::default();
+
+    settings.merge(config::File::with_name("configuration"))?;
+    settings.try_into()
+}
